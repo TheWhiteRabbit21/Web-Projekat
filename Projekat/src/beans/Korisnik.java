5f569212e914962782e9ee9ba8dd61df5944581f
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-06-14 10:51:37 +0200

    Model korisnika napravljen

--- Projekat/src/beans/Korisnik.java
+++ Projekat/src/beans/Korisnik.java
@@ -2,19 +2,100 @@ package beans;
 
 import java.io.Serializable;
 
+import enums.Pol;
+import enums.Uloga;
+
+@SuppressWarnings("serial")
 public class Korisnik implements Serializable{
 
 	private String korisnickoIme;
 	private String lozinka;
 	private String ime;
 	private String prezime;
-	private Boolean pol;
-	//private Date datumRodjenja;
-	//private Uloga uloga;
+	private Pol pol;
+	private String datumRodjenja;
+	private Uloga uloga;
+	private boolean deleted;
 	
-	
-	
-	
-	
+	public Korisnik() {
+		super();
+	}
+
+	public Korisnik(String korisnickoIme, String lozinka, String ime, String prezime, Pol pol, String datumRodjenja,
+			Uloga uloga, boolean deleted) {
+		super();
+		this.korisnickoIme = korisnickoIme;
+		this.lozinka = lozinka;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.pol = pol;
+		this.datumRodjenja = datumRodjenja;
+		this.uloga = uloga;
+		this.deleted = deleted;
+	}
+
+	public String getKorisnickoIme() {
+		return korisnickoIme;
+	}
+
+	public void setKorisnickoIme(String korisnickoIme) {
+		this.korisnickoIme = korisnickoIme;
+	}
+
+	public String getLozinka() {
+		return lozinka;
+	}
+
+	public void setLozinka(String lozinka) {
+		this.lozinka = lozinka;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public Pol getPol() {
+		return pol;
+	}
+
+	public void setPol(Pol pol) {
+		this.pol = pol;
+	}
+
+	public String getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(String datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	public Uloga getUloga() {
+		return uloga;
+	}
+
+	public void setUloga(Uloga uloga) {
+		this.uloga = uloga;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
 	
 }
