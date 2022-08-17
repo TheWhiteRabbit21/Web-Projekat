cfe1783f1f8c75de9c493cf6fd3c364bcdd50dec
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-08-17 16:27:59 +0200

    Rad na profil stranici i menjanju podataka za korisnika.

--- Projekat/WebContent/JavaScriptFiles/profilKorisnika.js
+++ Projekat/WebContent/JavaScriptFiles/profilKorisnika.js
@@ -1,3 +1,19 @@
-/**
- * 
- */
+var profilKorisnika = new Vue({
+	el:'#korisnickeInformacije',
+	data: {
+		korisnik: null,
+		mode: "BROWSE"
+	},
+	mounted () {
+        axios
+          .get('rest/login/currentUser')
+          .then(response => (this.korisnik = response.data))
+    },
+	
+	
+	
+	
+	
+	
+	
+});
